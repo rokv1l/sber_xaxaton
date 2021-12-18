6d665723e908fed6ac9196c3d3c05930cf628f5a
leo <guzeev.leonid.dc@gmail.com>
2021-12-18 14:15:37 +0300

    f

--- api/resources/route.py
+++ api/resources/route.py
@@ -5,4 +5,15 @@ from flask_restful import Resource, reqparse
 
 class Route(Resource):
     def get(self):
-        pass
+        parser = reqparse.RequestParser()
+        parser.add_argument('from', required=True)
+        parser.add_argument('to', required=True)
+        parser.add_argument('vehicle', default='foot')
+        
+        args = parser.parse_args() 
+        try:
+            args['from'] = list(map(float, args['from'].split(',')))
+            args['to'] = list(map(float, args['to'].split(',')))
+        except:
+            return {'error': 'invalid coordinates'}, 404
+
