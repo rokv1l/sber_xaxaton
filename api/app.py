d56f97de496e4ae3d05db1f4c39aef19a7c5ef26
leo <guzeev.leonid.dc@gmail.com>
2021-12-18 14:08:49 +0300

    hello

--- api/app.py
+++ api/app.py
@@ -3,11 +3,14 @@ from flask_cors import CORS
 from flask_restful import Api
 
 from src.db import session_maker
+from resources.hello import Hello
 
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
+
+api.add_resource(Hello, '/hello')
 
 
 if __name__ == '__main__':
