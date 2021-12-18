4c92972269da3b24a124cd9d82fb721a1287e366
leo <guzeev.leonid.dc@gmail.com>
2021-12-18 13:52:14 +0300

    init

---
+++ api/src/db.py
@@ -0,0 +1,24 @@
+from time import sleep
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine.url import URL
+from sqlalchemy.exc import OperationalError
+from sqlalchemy.ext.declarative import declarative_base
+
+import config
+
+base = declarative_base()
+
+
+data = {
+    'drivername': 'postgresql',
+    'host': config.db_host,
+    'port': config.db_port,
+    'username': config.db_username,
+    'password': config.db_password,
+    'database': config.db_name,
+}
+
+engine = create_engine(URL(**data))
+session_maker = sessionmaker(bind=engine)
